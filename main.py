a2fd3471eccab3fdac7977198209f0cee729bc6a
OZimmermann <82403411+OZimmermann@users.noreply.github.com>
2021-11-16 08:42:24 +0100

    Aktualisiere pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,28 +1,42 @@
 def pause2():
     basic.pause(100)
+
 def Herzschlag():
     basic.show_icon(IconNames.HEART)
     basic.pause(100)
     basic.show_icon(IconNames.SMALL_HEART)
     basic.pause(100)
+
 Herzschlag()
+Herzschlag()
+
+for zaehle in range(4):
+    Herzschlag() 
+
 if input.button_is_pressed(Button.A):
     basic.show_number(0)
+
 if input.button_is_pressed(Button.B):
     basic.show_number(1)
 else:
     basic.show_number(2)
+
 for index in range(4):
     basic.show_number(3)
+
 for index2 in range(5):
     basic.show_number(4)
+
 while input.button_is_pressed(Button.A):
     basic.show_number(5)
+
+for index3 in range(4):
+    Herzschlag()
+
+while True:
+    pause2()
+
 while input.button_is_pressed(Button.A):
     basic.show_number(6)
     if input.button_is_pressed(Button.B):
         break
-for index3 in range(4):
-    Herzschlag()
-while True:
-    pause2()
--- main.ts
+++ main.ts
@@ -1,39 +1,48 @@
-function pause2 () {
+function pause2() {
     basic.pause(100)
 }
-function Herzschlag () {
+
+function Herzschlag() {
     basic.showIcon(IconNames.Heart)
     basic.pause(100)
     basic.showIcon(IconNames.SmallHeart)
     basic.pause(100)
 }
+
 Herzschlag()
+Herzschlag()
+for (let zaehle = 0; zaehle < 4; zaehle++) {
+    Herzschlag()
+}
 if (input.buttonIsPressed(Button.A)) {
     basic.showNumber(0)
 }
+
 if (input.buttonIsPressed(Button.B)) {
     basic.showNumber(1)
 } else {
     basic.showNumber(2)
 }
+
 for (let index = 0; index < 4; index++) {
     basic.showNumber(3)
 }
-for (let index = 0; index < 5; index++) {
+for (let index2 = 0; index2 < 5; index2++) {
     basic.showNumber(4)
 }
 while (input.buttonIsPressed(Button.A)) {
     basic.showNumber(5)
 }
-while (input.buttonIsPressed(Button.A)) {
-    basic.showNumber(6)
-    if (input.buttonIsPressed(Button.B)) {
-        break;
-    }
-}
-for (let index = 0; index < 4; index++) {
+for (let index3 = 0; index3 < 4; index3++) {
     Herzschlag()
 }
 while (true) {
     pause2()
+}
+while (input.buttonIsPressed(Button.A)) {
+    basic.showNumber(6)
+    if (input.buttonIsPressed(Button.B)) {
+        break
+    }
+    
 }
